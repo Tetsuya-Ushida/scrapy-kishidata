972a9c1cc43fe9322b75073478a88d76da2568c7
Keiichiro Yamamoto <k.yanmar@gmail.com>
2020-02-02 04:14:44 +0900

    [debug]棋士個別ページでのデータ取得を確認

--- kishidata/spiders/kishi.py
+++ kishidata/spiders/kishi.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.loader import ItemLoader
 from kishidata.items import KishiWorkItem
+from scrapy.loader.processors import TakeFirst
 
 class KishiSpider(scrapy.Spider):
     name = 'kishi'
     allowed_domains = ['shogi.or.jp']
-    start_urls = ['https://www.shogi.or.jp/player/numerical_order.html']
+    #start_urls = ['https://www.shogi.or.jp/player/numerical_order.html']
+    #start_urls = ['https://www.shogi.or.jp/player/pro/307.html']
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.shogi.or.jp/player/pro/307.html', callback=self.parse_kishi)
 
     def parse(self, response):
         root_url = '/'.join(response.url.split('/')[:3])
         kishi_urls = response.xpath('//a[contains(@href,"player/pro")]/@href').extract()
         for path in kishi_urls:
-            print(root_url + path)
-            #yield scrapy.Request(root_url + path, callback=parse_kishi)
+            #print(root_url + path)
+            yield scrapy.Request(root_url + path, callback=self.parse_kishi)
 
     def parse_kishi(self, response):
-        kishi = ItemLoader(item=KishiWorkItem, response=response)
+        kishi = ItemLoader(item=KishiWorkItem(), response=response)
+        kishi.default_output_processor = TakeFirst()
         kishi.add_xpath('id', '//table[@class="tableElements02"]//th[text()="棋士番号"]//following-sibling::td//text()')
         kishi.add_xpath('name', '//div[@class="nameArea"]//h1[@class="nameTtl"]//span[@class="jp"]/text()')
         kishi.add_xpath('name_en', '//div[@class="nameArea"]//h1[@class="nameTtl"]//span[@class="en"]/text()')
